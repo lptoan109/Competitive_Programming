009de619e985ad78b32560b48d0eb62ad37925cb
LPToan <lephuoctoan6411@gmail.com>
2025-10-18 10:02:47 +0700

    update

--- HONGBANG_OJ/NANG_CAO/BTNC112_TimViTri/main.cpp
+++ HONGBANG_OJ/NANG_CAO/BTNC112_TimViTri/main.cpp
@@ -1,41 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-ll a[10000000];
-void bs(ll n, ll x){
-    ll l = 0, r = n-1,vt;
-    bool cl = true;
-    while(l<=r){
-        ll m = l+(r-l)/2;
-        if(a[m]==x){
-            while(a[m-=1]==x) vt = m;
-            cout << "Vi tri dau tien cua " << x << " trong mang A la " << vt << "\n";
-            cl=false;
-            break;
-        }
-        else if(a[m]<x) r=m+1;
-        else l = m-1;
+
+const int MAXN = 1e7;
+int a[MAXN + 5];
+int n, x;
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> n >> x;
+    for(int i = 1; i <= n; ++i){
+        cin >> a[i];
     }
-    if(cl) cout<< "No\nNo\n";
+    sort(a + 1, a + n + 1);
+
+    int low = lower_bound(a + 1, a + n + 1, x) - a,
+        high = upper_bound(a + 1, a + n + 1, x) - a;
+
+    if(find(a + 1, a + n + 1, x) == a + n + 1){
+        cout << "No\n";
+        cout << "No\n";
+    }
     else{
-        while(a[vt+1]==x) vt+=1;
-        cout << "Vi tri cuoi cung cua " << x << " trong mang A la " << vt << "\n";
+        cout << "Vi tri dau tien cua " << x << " trong mang A la " << low << "\n";
+        cout << "Vi tri cuoi cung cua " << x << " trong mang A la " << high - 1 << "\n";
     }
-    while(l<=r){
-        ll m = l+(r-l)/2;
-        if(a[m]>x){
-            cout <<" Vi tri cua phan tu dau tien > "<<x<<" trong mang A la " << r << ", gia tri " << a[r] << "\n";
-            break;
-        }
-        else l = m-1;
-    }
-}
-int main()
-{
-    ll n,x;
-    cin>>n>>x;
-    for(int i = 0; i<n; ++i) cin>>a[i];
-    sort(a,a+n);
-    bs(n,x);
+
+    if(high == n + 1) cout << "No\n";
+    else cout << "Vi tri cua phan tu dau tien > " << x << " trong mang A la " << high << ", gia tri " << a[high] << "\n";
+
     return 0;
 }
