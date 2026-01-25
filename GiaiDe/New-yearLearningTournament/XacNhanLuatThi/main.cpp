120f9fa6c504bf97db3ed150fbed24abd9e14c5e
LPToan <lephuoctoan6411@gmail.com>
2026-01-25 21:34:44 +0700

    update

---
+++ GiaiDe/New-yearLearningTournament/XacNhanLuatThi/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("OATH.INP", "r", stdin);
+    freopen("OATH.OUT", "w", stdout);
+    cout << "Yes, I've read and understood all the contest rules.";
+    return 0;
+}
