0ed3133c5fb08b0838e42704503b399c22b9ccad
LPToan <lephuoctoan6411@gmail.com>
2026-03-16 23:53:20 +0700

    update

--- HONGBANG_OJ/NANG_CAO/C11_QuyHoachDong/TroChoi/main.cpp
+++ HONGBANG_OJ/NANG_CAO/C11_QuyHoachDong/TroChoi/main.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 #define ll long long
 const int MAXN = 1e6;
-ll n,k, a[MAXN+5], dp[MAXN+5];
-int kvt[MAXN+5], kqvt;
-vector <int> kqv;
+ll n, k, a[MAXN+5], dp[MAXN+5], vtdp[MAXN+5], kq = -1e18, vmax;
+vector<ll>vkq;
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -15,8 +14,25 @@ int main()
     for(int i = 1; i<=n; ++i){
         cin >> a[i];
     }
-    for(int i = 1; i<=n; ++i){
-        ;
+    dp[1] = a[1];
+    for(ll i = 2; i<=n; ++i){
+        ll j = i-k, maxx = -1e18, vt;
+        if(j<1) j = 1;
+        for(; j<i; ++j){
+            if(dp[j]>=maxx){
+                maxx = dp[j];
+                vt = j;
+            }
+        }
+        dp[i] = maxx+a[i];
+        vtdp[i] = vt;
     }
+    for(ll i = 1; i<=n; ++i){
+        cout << dp[i] << " ";
+        kq = max(kq, dp[i]);
+    }
+//    while(i>1){
+//
+//    }
     return 0;
 }
