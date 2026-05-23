66c99b43bb6089e901849c502706a1d6696b85f4
LePhuocToan <lephuoctoan6411@gmail.com>
2026-05-23 23:40:27 +0700

    update

---
+++ GiaiDe/TS10TPHCM2324/Bai2_PhanLoai/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[10][10], temp1, temp2, temp3, kq1, kq2, kq3;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("PHANLOAI.INP", "r", stdin);
+    freopen("PHANLOAI.OUT", "w", stdout);
+
+    return 0;
+}
--- HONGBANG_OJ/CO_BAN/KTLoaiChuong8/QUYHOACH/main.cpp
+++ HONGBANG_OJ/CO_BAN/KTLoaiChuong8/QUYHOACH/main.cpp
@@ -1,36 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[1000005], ch[1000005];
-void nhap(int &n, int a[], int &h){
-    cin >> n >> h;
-    for(int i = 0; i<n; ++i){
-        cin >> a[i];
-    }
-}
-void xuat(int n, int a[]){
-    for(int i = 0; i<n; ++i){
-        cout << a[i] << " ";
-    }
-}
-void kt(int n, int a[], int h){
-    int d = 0;
-    for(int i = 0; i<n; ++i){
-        if(a[i]>h){
-            ch[d] = i;
-            ++d;
-        }
-    }
-    cout << d << "\n";
-    xuat(d,ch);
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cout.tie(0); cin.tie(0);
+int h, n, kq;
+vector <int> vkq;
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     freopen("QUYHOACH.INP", "r", stdin);
     freopen("QUYHOACH.OUT", "w", stdout);
-    int n, h;
-    nhap(n,a,h);
-    kt(n,a,h);
+    cin >> n >> h;
+    for(int i = 0; i<n; ++i){
+        int t;
+        cin >> t;
+        if(t>h){
+            kq++;
+            vkq.push_back(i);
+        }
+    }
+    cout << kq << "\n";
+    for(int i : vkq) cout << i << " ";
     return 0;
 }
