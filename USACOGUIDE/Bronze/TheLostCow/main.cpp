42e119e80cca2699c1d99b378dd07e44acbb5a19
LePhuocToan <lephuoctoan6411@gmail.com>
2026-06-07 21:19:42 +0700

    update

---
+++ USACOGUIDE/Bronze/TheLostCow/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int x, y
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    freopen("lostcow.in", "r", stdin);
+    freopen("lostcow.out", "w", stdout);
+
+    return 0;
+}
